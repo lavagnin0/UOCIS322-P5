b4debe1f645b78689389980aa4e63bd339f2e7c3
lavagnin0 <77555983+lavagnin0@users.noreply.github.com>
2021-05-23 20:59:17 -0700

    Update test_acp_times.py

--- brevets/tests/test_acp_times.py
+++ brevets/tests/test_acp_times.py
@@ -1,6 +1,9 @@
+"""
+Test the solution acp_times just to be super sure
+"""
 import nose
 import arrow
-from brevets import acp_times
+import acp_times
 
 sample_time = arrow.get('01/01/2021 00:00:00', 'MM/DD/YYYY HH:mm:ss')
 
@@ -9,67 +12,67 @@ def basic_test_open():
     """
     Example open times from the algorithm description webpage
     """
-    assert acp_times.open_time(60, 200, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=1,
-                                                                                                           minutes=46)
-    assert acp_times.open_time(120, 200, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=3,
-                                                                                                            minutes=32)
-    assert acp_times.open_time(175, 200, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=5,
-                                                                                                            minutes=9)
-    assert acp_times.open_time(200, 200, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=5,
-                                                                                                            minutes=53)
+    assert arrow.get(acp_times.open_time(60, 200, sample_time)) == sample_time.shift(hours=1,
+                                                                          minutes=46)
+    assert arrow.get(acp_times.open_time(120, 200, sample_time)) == sample_time.shift(hours=3,
+                                                                           minutes=32)
+    assert arrow.get(acp_times.open_time(175, 200, sample_time)) == sample_time.shift(hours=5,
+                                                                           minutes=9)
+    assert arrow.get(acp_times.open_time(200, 200, sample_time)) == sample_time.shift(hours=5,
+                                                                           minutes=53)
 
 
 def basic_test_close():
     """
-    Example close times from the algorithm description webpage
+    Example open times from the algorithm description webpage
     """
-    assert acp_times.close_time(60, 200, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=4)
-    assert acp_times.close_time(120, 200, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=8)
-    assert acp_times.close_time(175, 200, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=11,
-                                                                                                             minutes=40)
+    assert arrow.get(acp_times.close_time(60, 200, sample_time)) == sample_time.shift(hours=4)
+    assert arrow.get(acp_times.close_time(120, 200, sample_time)) == sample_time.shift(hours=8)
+    assert arrow.get(acp_times.close_time(175, 200, sample_time)) == sample_time.shift(hours=11,
+                                                                            minutes=40)
 
 
 def longer_distance_open():
     """
     Testing open times at distances farther than 200km, taken from the algorithm description webpage
     """
-    assert acp_times.open_time(350, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=10,
-                                                                                                             minutes=34)
-    assert acp_times.open_time(550, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=17,
-                                                                                                             minutes=8)
-    assert acp_times.open_time(890, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=29,
-                                                                                                             minutes=9)
+    assert arrow.get(acp_times.open_time(350, 1000, sample_time)) == sample_time.shift(hours=10,
+                                                                            minutes=34)
+    assert arrow.get(acp_times.open_time(550, 1000, sample_time)) == sample_time.shift(hours=17,
+                                                                            minutes=8)
+    assert arrow.get(acp_times.open_time(890, 1000, sample_time)) == sample_time.shift(hours=29,
+                                                                            minutes=9)
 
 
 def longer_distance_close():
     """
     Testing close times at distances farther than 200km, taken from the algorithm description webpage
     """
-    assert acp_times.close_time(550, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=36,
-                                                                                                              minutes=40)
-    assert acp_times.close_time(600, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=40)
-    assert acp_times.close_time(890, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=65,
-                                                                                                              minutes=23)
+    assert arrow.get(acp_times.close_time(550, 1000, sample_time)) == sample_time.shift(hours=36,
+                                                                             minutes=40)
+    assert arrow.get(acp_times.close_time(600, 1000, sample_time)) == sample_time.shift(hours=40)
+    assert arrow.get(acp_times.close_time(890, 1000, sample_time)) == sample_time.shift(hours=65,
+                                                                             minutes=23)
 
 
 def test_controle_close_to_start():
     """
     Tests open/close times for controle locations for 60 km and under
 	"""
-    assert acp_times.open_time(0, 1000, sample_time).replace(second=0, microsecond=0) == sample_time
-    assert acp_times.close_time(0, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=1)
+    assert arrow.get(acp_times.open_time(0, 1000, sample_time)) == sample_time
+    assert arrow.get(acp_times.close_time(0, 1000, sample_time)) == sample_time.shift(hours=1)
 
-    assert acp_times.open_time(5, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(minutes=9)
-    assert acp_times.close_time(5, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=1,
-                                                                                                            minutes=15)
+    assert arrow.get(acp_times.open_time(5, 1000, sample_time)) == sample_time.shift(minutes=9)
+    assert arrow.get(acp_times.close_time(5, 1000, sample_time)) == sample_time.shift(hours=1,
+                                                                           minutes=15)
 
-    assert acp_times.open_time(20, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(minutes=35)
-    assert acp_times.close_time(20, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=2)
+    assert arrow.get(acp_times.open_time(20, 1000, sample_time)) == sample_time.shift(minutes=35)
+    assert arrow.get(acp_times.close_time(20, 1000, sample_time)) == sample_time.shift(hours=2)
 
-    assert acp_times.open_time(30, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(minutes=53)
-    assert acp_times.close_time(30, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=2,
-                                                                                                             minutes=30)
+    assert arrow.get(acp_times.open_time(30, 1000, sample_time)) == sample_time.shift(minutes=53)
+    assert arrow.get(acp_times.close_time(30, 1000, sample_time)) == sample_time.shift(hours=2,
+                                                                            minutes=30)
 
-    assert acp_times.open_time(60, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=1,
-                                                                                                            minutes=46)
-    assert acp_times.close_time(60, 1000, sample_time).replace(second=0, microsecond=0) == sample_time.shift(hours=4)
+    assert arrow.get(acp_times.open_time(60, 1000, sample_time)) == sample_time.shift(hours=1,
+                                                                           minutes=46)
+    assert arrow.get(acp_times.close_time(60, 1000, sample_time)) == sample_time.shift(hours=4)
